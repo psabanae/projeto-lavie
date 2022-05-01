1fef21fc22e944c5db5cc2482ed7af740ede8fd2
RamonoesQ <ramonqueirozs@gmail.com>
2022-05-01 09:16:35 -0300

    att nome do arquivo

---
+++ src/routes/psicologosRoutes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const controllerPsicologos = require("../controllers/controllerPsicologos");
+const routes = express.Router();
+
+routes.get("/psicologos", controllerPsicologos.listarPsicologos);
+routes.get("/psicologos/:id", controllerPsicologos.listarPsicologosId);
+routes.delete("/psicologos/:id", controllerPsicologos.deletarPsicologo)
+
+module.exports = routes;
