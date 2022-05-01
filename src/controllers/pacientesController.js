388bd70ed0bf63df27508ab616464fb2e000eb32
Laura <arq.lauracordeiro@gmail.com>
2022-05-01 11:06:03 -0300

    CRUD pacientes partially done. Added Validator.

--- src/controllers/pacientesController.js
+++ src/controllers/pacientesController.js
@@ -1,45 +1,44 @@
 const Pacientes = require("../models/Pacientes");
 
 const PacientesController = {
-
   //Lista todos pacientes
   async listar(req, res) {
-      try {
-        const pacientes = await Pacientes.findAll();
-        return res.status(200).json(pacientes);
-      } catch (error) {
-        return res.status(500).json(error.message);
-      }
-    /*async listar(req, res) {
-    const pacientes = await Pacientes.findAll();
-    res.json(pacientes);
-  }*/
+    try {
+      const pacientes = await Pacientes.findAll();
+      return res.status(200).json(pacientes);
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
   },
 
   //Lista paciente por Id
   async recuperar(req, res) {
-      const { id } = req.params;
+    const { id } = req.params;
 
-      if(!id) return res.status(400).json("Id não enviada.");
+    if (!id) return res.status(400).json("Id não enviada.");
 
-      try {
-        const paciente = await Pacientes.findOne({
-            where: {id_pacientes: id}}
-          );
-        if (paciente.length === 0) {
-          return res.status(404).json("Id do paciente não encontrada.");
-        }
-        
-        return res.status(200).json(paciente);
-
-      } catch (error) {
-        return res.status(500).json("Erro Inesperado");
+    try {
+      const paciente = await Pacientes.findOne({
+        where: { id_pacientes: id }
+      });
+      if (paciente.length === 0) {
+        return res.status(404).json("Id do paciente não encontrada.");
       }
+
+      return res.status(200).json(paciente);
+    } catch (error) {
+      return res.status(500).json("Erro Inesperado");
+    }
   },
 
   //Cadastra paciente
   async cadastrarPacientes(req, res) {
     const { nome, email, nascimento, status } = req.body;
+    if (!nome || !email || !nascimento || !status) {
+      return res
+        .status(400)
+        .json({ error: "Você precisa passar os atributos corretamente" });
+    }
     const novoPaciente = await Pacientes.create({
       nome,
       email,
@@ -54,39 +53,18 @@ const PacientesController = {
     const { id } = req.params;
     const { novo_nome, novo_email, nova_dataNascimento } = req.body;
 
-    if (!id) return res.status(400).json('Id não enviada.');
+    if (!id) return res.status(400).json("Id não enviada.");
 
     try {
       const atualizarPaciente = await Pacientes.update(
-        { nome: novo_nome,
-          email: novo_email,
-          nascimento: nova_dataNascimento },
-        { where: {id_pacientes: id} }
+        { nome: novo_nome, email: novo_email, nascimento: nova_dataNascimento },
+        { where: { id_pacientes: id } }
       );
-      return res.status(200).json('Paciente Atualizado');
+      return res.status(200).json("Paciente Atualizado");
     } catch (error) {
-      return res.status(400).json('Erro Inesperado');
+      return res.status(400).json("Erro Inesperado");
     }
-  },
-
+  }
 };
 
 module.exports = PacientesController;
-
-/*
-  async cadastrarPacientes(req, res) {
-    const { nome, email, idade, status } = req.body;
-    if (!nome || !email || !idade || !status) {
-      const novoPaciente = await Pacientes.create({
-        nome,
-        email,
-        idade,
-        status
-      });
-      return res
-        .status(400)
-        .json({ error: "Você precisa passar os atributos corretamente" });
-    }
-    res.status(201).json({ message: "Cadastro efetuado com sucesso" });
-  }
-};*/
