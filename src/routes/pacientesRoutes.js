37dbaa8fe74bf25fb51ce41ab9b666459fe52644
RamonoesQ <ramonqueirozs@gmail.com>
2022-05-04 19:59:01 -0300

    verificação de código

--- src/routes/pacientesRoutes.js
+++ src/routes/pacientesRoutes.js
@@ -2,13 +2,13 @@ const express = require("express");
 const routes = express.Router();
 
 const PacientesController = require("../controllers/pacientesController");
-const pacientesValidator = require("../validator/pacientesValidacao");
+const PacientesValidator = require("../validator/pacientesValidacao");
 
 
 routes.get("/pacientes", PacientesController.listarTodosPacientes);
 routes.get("/pacientes/:id", PacientesController.listarPaciente);
-routes.post("/pacientes", pacientesValidator, PacientesController.cadastrarPaciente);
-routes.put("/pacientes/:id", pacientesValidator, PacientesController.atualizarPaciente);
+routes.post("/pacientes", PacientesValidator, PacientesController.cadastrarPaciente);
+routes.put("/pacientes/:id", PacientesValidator, PacientesController.atualizarPaciente);
 routes.delete("/pacientes/:id", PacientesController.deletarPaciente);
 
 
